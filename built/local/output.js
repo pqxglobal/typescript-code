53b9521a469d91faedabd7f86dcf31da3ba07857
Leo Slegt <86182182+pqxglobal@users.noreply.github.com>
2022-06-27 17:09:53 +0200

    Delete output.js

--- built/local/output.js
+++
@@ -1,9 +0,0 @@
-var EyeColor;
-(function (EyeColor) {
-    EyeColor[EyeColor["Brown"] = 1] = "Brown";
-    EyeColor[EyeColor["Green"] = 5] = "Green";
-    EyeColor[EyeColor["Blue"] = 10] = "Blue";
-})(EyeColor || (EyeColor = {}));
-var myEyeColor = EyeColor.Brown;
-console.log(myEyeColor);
-// console.log(myEyeColor[myEyeColor]);
