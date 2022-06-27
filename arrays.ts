e589fdc116219b749045ce09551d2e115686ed9c
pqx <l.r.slegt@gmail.com>
2022-06-27 17:34:25 +0200

    add tuples

---
+++ arrays.ts
@@ -0,0 +1,8 @@
+let strArr1: string[] = ['Hello ', 'world'];
+let strArr2: Array<string> = ['Hello ', 'world'];
+
+let anyArr: any[] = ['Hello', 10, true];
+
+console.log(strArr1);
+console.log(strArr2);
+console.log(anyArr);
---
+++ tuples.ts
@@ -0,0 +1,6 @@
+let myTuple: [string, number, number] = ['Hi', 10, 0];
+
+console.log(myTuple[0]);
+console.log(myTuple[1]);
+myTuple[2] = 100;
+console.log(myTuple[2]);
